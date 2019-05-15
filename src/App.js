0c7edff77a826f432764377039e948fc8d4963e9
Mitya Ustinov <dmitry.ustinov@accenture.com>
2019-05-15 12:05:16 +0300

    cleanup, structure update

--- src/App.js
+++ src/App.js
@@ -1,121 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
 import queryString from 'query-string'
 
-// let fakeServerData = {
-//     user: {
-//         name: "Mitya",
-//         playlists: [
-//             {
-//                 name: 'The Beatles',
-//                 image: require ('./beatles.jpg'),
-//                 songs: [
-//                     {name: 'Hey Jude', duration: 234},
-//                     {name: 'I Wanna Hold Your Hand', duration: 303},
-//                     {name: 'Paperback Writer', duration: 238},
-//                     {name: 'Ob-la-di Ob-la-da', duration: 193},
-//                     {name: 'Penny Lane', duration: 271}
-//                 ]
-//             },
-//             {
-//                 name: 'Morcheeba',
-//                 image: require ('./morcheeba.jpg'),
-//                 songs: [
-//                     {name: 'Charango', duration: 234},
-//                     {name: 'Blood Like Lemonade', duration: 303},
-//                     {name: 'The Great London Traffic Warden Massacre', duration: 238},
-//                     {name: 'Rome wasn\'t built in a day', duration: 193},
-//                     {name: 'Even though', duration: 271}
-//                 ]
-//             },
-//             {
-//                 name: 'The Prodigy',
-//                 image: require ('./prodigy.jpg'),
-//                 songs: [
-//                     {name: 'Smack My Bitch Up', duration: 234},
-//                     {name: 'Firestarter', duration: 303},
-//                     {name: 'Breathe', duration: 238},
-//                     {name: 'Wolrd\'s On Fire', duration: 193},
-//                     {name: 'No Army Man', duration: 271}
-//                 ]
-//             },
-//             {
-//                 name: 'M.A.V.S.',
-//                 image: require ('./mavs.jpg'),
-//                 songs: [
-//                     {name: 'Implant', duration: 234},
-//                     {name: 'Brigador Theme', duration: 303},
-//                     {name: 'A Glowing Light, a Promice', duration: 238},
-//                     {name: 'Wavehymnal', duration: 193},
-//                     {name: 'Richter', duration: 271}
-//                 ]
-//             },
-//         ]
-//     }
-// };
+import PlaylistCounter from './components/PlaylistCounter'
+import HoursCounter from './components/HoursCounter'
+import Filter from './components/Filter'
+import Playlist from './components/Playlist'
 
-class PlaylistCounter extends Component {
-    render() {
-        let playlistAmountLabel = null;
-        if (this.props.playlists.length !== 1) {
-            playlistAmountLabel = 'playlists';
-        } else {
-            playlistAmountLabel = 'playlist';
-        }
-
-        return (
-            <div style={{display: 'inline-block'}}>
-                <h2>{this.props.playlists && this.props.playlists.length} {playlistAmountLabel}</h2>
-            </div>
-        );
-    }
-}
-
-class HoursCounter extends Component {
-    render() {
-        let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
-            return songs.concat(eachPlaylist.songs)
-        }, []);
-
-        let totalDuration = allSongs.reduce((sum, eachSong) => {
-           return sum + eachSong.duration
-        }, 0)
-
-        return (
-            <div style={{width: "40%", display: 'inline-block'}}>
-                <h2>{(totalDuration/3600).toFixed(1)} hours</h2>
-            </div>
-        );
-    }
-}
-
-class Filter extends Component {
-    render() {
-        return (
-            <div className="filter">
-                <input type="search" placeholder="Filter" onKeyUp={event =>
-                    this.props.onTextChange(event.target.value)} />
-            </div>
-        );
-    }
-}
-
-class Playlist extends Component {
-    render() {
-        let playlist = this.props.playlist;
-        return (
-            <div className="playlist-item">
-                <img src={playlist.imageUrl} alt={playlist.name} />
-                <h3>{playlist.name}</h3>
-                <ol>
-                    {playlist.songs.map(song =>
-                        <li>{song.name}</li>
-                    )}
-                </ol>
-            </div>
-        );
-    }
-}
+import './assets/styles/App.css';
 
 class App extends Component {
     constructor () {
@@ -125,20 +16,14 @@ class App extends Component {
             filterString: ''
         }
     }
+
     componentDidMount () {
-        // setTimeout (() => {
-        //     this.setState({serverData: fakeServerData});
-        // }, 500);
-        //setTimeout (() => {
-        //    this.setState({filterString: ''});
-        //}, 3000);
         let parsed = queryString.parse(window.location.search); // taking access_token from Url
-        console.log(parsed);
+        // console.log(parsed);
         // let token = new URLSearchParams(window.location.search).get('access_token') // another way of taking access_token from Url without queryString plugin
         let accessToken = parsed.access_token;
         console.log(accessToken);
 
-        //const proxyUrl = 'https://cors-anywhere-mitya.herokuapp.com/' //proxy Url to fix CORS problem
         const apiUrlMe = 'https://api.spotify.com/v1/me' //Spotify API User
         const apiUrlPlaylists = 'https://api.spotify.com/v1/me/playlists' //Spotify API Url Playlists
 
@@ -184,10 +69,8 @@ class App extends Component {
             })
             return playlistsPromise
         })
-
         .then(playlists => this.setState({
             playlists: playlists.map(item => {
-                // console.log(item.trackDatas)
                 if(item.images.length > 0){
                     return {
                         imageUrl: item.images[0].url,
@@ -204,6 +87,7 @@ class App extends Component {
             })
         }))
     }
+
     render() {
         let playlistsToRender =
             this.state.user &&
